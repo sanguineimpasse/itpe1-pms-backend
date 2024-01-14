a14302b47bbd526631e5ee4c303f63d77d5e8473
sanguineimpasse <10geneius@gmail.com>
2024-01-14 17:57:48 +0800

    login functions update

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
 const {
   createUser,
   checkUserCode,
-  login,
-  findAllUsers,
-  findUserID,
-  updateUser
+  login
 } = require("./crudRoutes");
 
 function getRoutes(app) {
   app.post("/create/user", createUser);
   app.post("/login", login);
   app.post("/verify/usercode", checkUserCode);
-  // app.post("/findAll/users", findAllUsers);
-  // app.post("/find/user-id", findUserID);
-  // app.post("/update/user", updateUser);
+
+  app.get('*', function(req, res){
+    res.status(404).send('incorrect route');
+    unknownRoute();
+
+  });
+  app.post('*', function(req, res){
+    res.status(404).send('incorrect route');
+    unknownRoute();
+  });
+}
+
+function unknownRoute(){
+  console.log('A client tried to access a non-existent route');
 }
 
 exports.getRoutes = getRoutes;
